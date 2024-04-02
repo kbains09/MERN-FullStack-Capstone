1a118d4e2fdc44aaf041b43a0ca6ec06e610a369
Kirat Bains <kiratbains09@gmail.com>
2024-04-01 23:31:47 -0700

    feat: hooks

---
+++ client/src/hooks/useFetchProducts.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const useFetchProducts = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await axios.get('http://localhost:8080/products');
+        setProducts(response.data);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  return { products, loading, error };
+};
+
+export default useFetchProducts;
